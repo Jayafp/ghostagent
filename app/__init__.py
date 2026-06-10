7bd95964690540ce775735fd5bc3e38abac6af78
Jaya <jiaye@Jayas-MacBook-Pro.local>
2026-06-10 08:58:20 +0800

    update app.__init__, add llm log

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
+import os
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
 from app.log.logger import LOG
-LOG.info('init app module success...')
+api_key = os.getenv("ANTHROPIC_API_KEY")
+if api_key:
+    api_key = api_key[:5] + "***" + api_key[-5:]
+print(api_key)
+LOG.info(f'init app module success, api_url={os.getenv("ANTHROPIC_BASE_URL")}, api_key={api_key}, model_id={os.getenv("MODEL_ID")}')
